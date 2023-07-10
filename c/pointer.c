d7c6bfdf3eacfb8f1a933a7dedf76fc2dc5bf0f4
bluebeaggle <shew1903@gmail.com>
2023-07-10 17:40:35 +0900

    Update pointer

--- c/pointer.c
+++ c/pointer.c
@@ -2,6 +2,17 @@
 
 int main(void) {
 
+    int a ;
+    a = 2;
+
+    printf("%p \n", &a) ;
+    
+    
+    int *p;
+    p = &a;
+
+    printf("포인터 p에 들어가 있는 값 : %p \n", p);
+    printf("int변수 a가 저장된 주소 : &p \n", &a);
 
     return 0;
 }
