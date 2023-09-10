28176d069a43d81e82df4c9a7c993a3011f6336b
bluebeaggle <shew1903@gmail.com>
2023-09-10 14:10:29 +0000

    buffer

---
+++ c/stdin.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(void) {
+	int num;
+	char c;
+
+	printf("숫자를 입력하세요: ");
+	scanf("%d", &num);
+
+	printf("문자를 입력하세요 : ");
+	scanf("%c", &c);
+
+
+
+
+
+	return 0;
+
+}
+
--- c/string.c
+++ c/string.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 
+int main(void) {
+
+	char null_1 = '\0';
+	char null_2 = 0;
+
+
+	char sentence_1[4] = {'Y', 'D', 'H', 0 };
+
+	printf("sentence_1 = %s", sentence_1);
+
+
+
+
+
+
+	return 0;
+}
+
